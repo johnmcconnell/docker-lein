b95e519c1eea570f7b1444000e415d48a37babe1
John McConnell <johnnyillinois@gmail.com>
2017-02-11 21:46:36 -0800

    Version 2.0.0

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject api "2.0.0"
+  :min-lein-version "2.6.1"
+  :description "Optly API"
+  :url "https://github.com/optly/api"
+  :license {:name "MIT License"
+            :url "https://mit-license.org"}
+  :release-tasks [["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  :target-path "target/%s")
